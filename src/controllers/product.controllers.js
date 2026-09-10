ae2519798a50b6315edf56710f3693cc17c09c00
Hardhikshettyr <hardhikshetty1771@gmail.com>
2026-09-10 14:35:02 +0530

    Adding required controllers for the project

--- src/controllers/product.controllers.js
+++ src/controllers/product.controllers.js
@@ -47,5 +47,117 @@ async function createProducts(req,res){
         }
     })
 }
+async function getProducts(req,res){
+    const{search,category,minPrice,maxPrice,sort,page=1,limit=10}=req.query;
+    let filter={};
 
-module.exports={createProducts};
+    if(search){
+        filter.name={
+            $regex:search,
+            $options:"i",
+        };
+    }
+
+    if(category){
+        filter.category=category;
+    }
+
+    if(minPrice || maxPrice){
+        filter.price={};
+
+        if(minPrice){
+            filter.price.$gte=Number(minPrice);
+        }
+        if(maxPrice){
+            filter.price.$lte=Number(maxPrice);
+        }
+    }
+
+    const pageNumber=Number(page);
+    const limitNumber=Number(limit);
+    const skip=(pageNumber-1)*limitNumber;
+
+    let sortOption={};
+
+    if(sort==="price_asc"){
+        sortOption.price=1;
+    }else if(sort==="price_desc"){
+        sortOption.price=-1;
+    }else if(sort==="name_asc"){
+        sortOption.name=1;
+    }else if(sort==="name_desc"){
+        sortOption.name=-1
+    }else{
+        sortOption.name=1;
+    }
+
+    const products=await productModel.find(filter)
+    .populate("category" ,"name")
+    .populate("seller","username email")
+    .sort(sortOption)
+    .skip(skip)
+    .limit(limitNumber)
+
+    const totalProducts=await productModel.countDocuments(filter);
+
+    res.status(200).json({
+        message:"Products fetched Successfully",
+        products,
+        pagination:{
+            currentPage:pageNumber,
+            limit:limitNumber,
+            totalProducts,
+            toatlPages:Math.ceil(totalProducts/limitNumber)
+        }
+    });
+    
+}
+async function getProductById(req,res){
+    const id=req.params.id;
+    const product=await productModel.findById(id);
+    if(!product){
+        return res.status(404).json({
+            message:"Product not found"
+        })
+    }
+    res.status(200).json({
+        message:"Product displayed Successfully",
+        product
+    })
+}
+async function updateProduct(req,res){
+    const _id=req.params.id
+    const seller=req.user.id
+    const product=await productModel.findOneAndUpdate(
+        {_id,seller},
+        req.body,
+        {returnDocument: "after"}
+    )
+    if(!product){
+        return res.status(404).json({
+            message:"product not found or You are not authorized"
+        })
+    }
+    res.status(200).json({
+        message:"product updated Successfully",
+        product,
+    })
+}
+async function deleteProduct(req,res){
+    const _id=req.params.id
+    const seller=req.user.id
+    const product=await productModel.findOneAndDelete({
+        _id,seller
+    })
+    if(!product){
+        return res.status(404).json({
+            message:"product not found or You are not authorized"
+        })
+    }
+    res.status(200).json({
+        message:"product deleted Successfully",
+
+    })
+}
+
+module.exports={createProducts,getProducts,getProductById,updateProduct,deleteProduct};
