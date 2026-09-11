7f61779319260d4ed2b6f1728d8bad096aa3933e
Hardhikshettyr <hardhikshetty1771@gmail.com>
2026-09-11 11:41:05 +0530

    adding required models for project

---
+++ src/models/cart.model.js
@@ -0,0 +1,30 @@
+const mongoose=require("mongoose");
+
+const cartSchema=new mongoose.Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"user",
+        required:true
+    },
+    items:[
+        {
+            product:{
+                type:mongoose.Schema.Types.ObjectId,
+                ref:"product",
+                required:true,
+            },
+            quantity:{
+                type:Number,
+                required:true,
+            },
+            price:{
+                type:Number,
+                required:true
+            },
+        }
+    ],
+});
+
+const cartModel=mongoose.model("cart",cartSchema);
+
+module.exports=cartModel;
