15f308046e7d6f320ac33407a287277d18359885
Hardhikshettyr <hardhikshetty1771@gmail.com>
2026-09-11 11:42:09 +0530

    adding app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express=require("express");
 const authroutes=require("./routes/auth.routes");
 const categoryroutes=require("./routes/categories.routes");
-const productRoutes=require("./routes/product.routes")
+const productRoutes=require("./routes/product.routes");
+const cartRoutes=require("./routes/cart.routes");
+
 const cookieparser=require("cookie-parser")
 const app=express();
 
@@ -11,5 +13,6 @@ app.use(cookieparser());
 app.use("/api/auth",authroutes);
 app.use("/api/categories",categoryroutes);
 app.use("/api/product",productRoutes)
+app.use("/api/cart",cartRoutes);
 
 module.exports=app;
